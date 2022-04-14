067b4ff0f628974e8a3a884aa7af60eeb5a72a30
vishalb172 <82908295+vishalb172@users.noreply.github.com>
2022-04-14 10:40:49 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+$hostname  = 'remotemysql.com';
+$username = 'EkhqiYcxgc';
+$password='8FfjvnjzaF';
+$dbname = 'train';
+$connect =  mysqli_connect($hostname , $username , $password ,$dbname) or die("Error Connecting");
+?>
